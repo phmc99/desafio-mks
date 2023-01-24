b2db04f3f640880f208fc511dede44d657eeb303
Pedro Costa <phmc99@outlook.com>
2023-01-23 23:39:40 -0300

    first commit

---
+++ src/pages/_document.tsx
@@ -0,0 +1,30 @@
+import NextDocument, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document';
+
+class MyDocument extends NextDocument {
+  static getInitialProps(ctx: DocumentContext) {
+    return NextDocument.getInitialProps(ctx);
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="description" content="Desafio MKS" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
