d745278f107150fd3f266c72171ad50790e0429c
Pedro Costa <phmc99@outlook.com>
2023-01-27 05:16:57 -0300

    feat: adding one test case

---
+++ src/test/App.spec.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import Home from '../pages/index';
+import '@testing-library/jest-dom';
+
+describe('Home', () => {
+  it('renders a heading', () => {
+    render(<Home />);
+
+    const title = screen.getByText('MKS');
+
+    expect(title).toBeTruthy();
+  });
+});
